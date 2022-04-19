47ddf51df3b6ceb5205da4df56054ef164d1bc52
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-04-19 16:42:01 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,12 @@
 //This is a small extension for using deltaTime in blocks. This allows for slow projects to run not in slow motion.
 //Thanks to KWX because I followed how he used delta in MakeCode. I knew what deltatime was from my using of Unity, but I didn't know how to implement it into MakeCode.
+let timeLastFrame = 0
 namespace Delta {
     //%block=Delta
     export function DELTA() : number {
-        return control.eventContext().deltaTime + 1
+        return (game.runtime() -timeLastFrame) / 1000
     }
 }
+game.onUpdate(function() {
+    timeLastFrame = game.runtime()
+})
