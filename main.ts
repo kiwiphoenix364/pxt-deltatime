5ed4a1cfcdf83f7c4c8a859fd1a440f8d128e4f9
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-04-19 17:11:15 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,10 +3,6 @@ let timeLastFrame = 0
 namespace Delta {
     //%block=Delta
     export function DELTA() : number {
-        scene.CONTROLLER_PRIORITY,(function() {
-    timeLastFrame = game.runtime()
-})
-        return ((game.runtime() - timeLastFrame) / 20) + 1
-        
+        return control.eventContext().deltaTime
     }
 }
