99b2d7dec7dd04ebc0697255633f4930b2ab9b9f
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-04-21 16:57:46 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,6 +3,6 @@ let timeLastFrame = 0
 namespace Delta {
     //%block=Delta
     export function DELTA() : number {
-        return control.eventContext().deltaTime * 40 + 0.15
+        return control.eventContext().deltaTime * 25 + 0.45
     }
 }
