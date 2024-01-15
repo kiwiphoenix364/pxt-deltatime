cdc98fe7ceddac572bbd102315f234b1c48a61a3
Kiwiphoenix <kiwiphoenix364@gmail.com>
2024-01-15 15:01:32 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,12 @@
 //This is a small extension for using deltaTime in blocks. This allows for slow projects to run not in slow motion.
 let timeLastFrame = 0
 namespace Delta {
-    //%block=Delta
+    //%block=delta
     export function DELTA() : number {
         return control.eventContext().deltaTime * 30
+    }
+    //%block='rawdelta'
+    export function RAW(): number {
+        return control.eventContext().deltaTime
     }
 }
